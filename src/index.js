74435e4fa4c5789b0cbe04ca0dbeb639080047cb
2883857@student.edu.az <2883857@student.edu.az>
2023-11-28 16:08:50 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,176 @@
+import {initializeApp } from "firebase/app";
+import {
+    collection,
+    onSnapshot,
+    getFirestore,
+    addDoc, deleteDoc, doc
+ } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCchalPMe4QFZXzkG8A7L1h1CtRXEZOook",
+    authDomain: "todolisteafb.firebaseapp.com",
+    projectId: "todolisteafb",
+    storageBucket: "todolisteafb.appspot.com",
+    messagingSenderId: "352113117273",
+    appId: "1:352113117273:web:e438ea9c4d7d93cb678948"
+  };
+
+
+//   init firebase app
+initializeApp(firebaseConfig);
+
+//   init services
+const db = getFirestore();
+
+//collection ref
+const colRef = collection(db, 'todos')
+
+//real time collection data
+onSnapshot(colRef, (snapshot)=>{
+  let todos = [];
+  // console.log(snapshot.docs);
+  snapshot.docs.forEach((doc)=>{
+    todos.push({...doc.data(), id: doc.id})
+  })
+
+  console.log(todos);
+})
+
+
+let btn = document.querySelector("#btn");
+let addPlace = document.querySelector("#addPlace");
+let inp = document.querySelector("#inp");
+
+btn.addEventListener("click", (e)=>{
+  e.preventDefault();
+
+  addDoc(colRef, {
+    todo: inp.value,
+  })
+  .then(()=>{
+    inp.value = "";
+  })
+  
+})
+
+
+let del = document.querySelector("#del");
+let delInp = document.querySelector("#delInp");
+
+del.addEventListener("click", (e)=>{
+  e.preventDefault();
+
+  const docRef = doc(db, "todos", delInp.value)
+
+  deleteDoc(docRef);
+
+})
+
+
+
+// console.log("salam");
+
+// function del(i) {   /// silme funksiyasi
+
+//     data.splice(i, 1);
+
+//     let datamap = data.reverse().map(function (el, i) {
+//         return ` <div class="d-flex justify-content-between">
+//         <div class="bg-primary rounded-2 my-3">${el}</div>
+//         <div class="d-flex justify-content-between g-2">
+
+//             <button class="bg-danger rounded-2 cursor-pointer" onclick="del(${i})">Delete</button>
+//             <button class="bg-warning rounded-2 cursor-pointer" onclick="edit(${i})">Edit</button>
+
+//         </div>
+//     </div>`;
+//     }).join("");
+
+//     result.innerHTML = datamap;
+// }
+
+ 
+
+// function update(e) {    //update funksiyasi
+ 
+//     console.log(e);
+
+//     // data.splice(index, 1, elem[0]);
+
+//     let datamap = data.map(function (el, i) {
+
+//         return ` <div class="d-flex justify-content-between">  
+        
+//         <div class="bg-primary rounded-2 my-3">${el}</div>
+           
+//             <div  class="d-flex justify-content-between g-2">
+    
+//                 <button class="bg-danger rounded-2 cursor-pointer" onclick="del(${i})">Delete</button>
+//                 <button class="bg-warning rounded-2 cursor-pointer" onclick="edit(${i})">Edit</button>
+//                 </div>
+           
+//             </div>`;
+//     }).join("");
+
+//     result.innerHTML = datamap;
+// }
+
+
+
+// function edit(index) {   /// edit funksiyasi
+
+//     let datamap = data.map(function (el, i) {
+
+//         if (index == i) {
+             
+//             return ` <div class="d-flex justify-content-between"> 
+//             <input id="inp1" class="rounded-2" type="text" value onchange="update(${i})"/>
+//             <div  class="d-flex justify-content-between g-2">
+    
+//                 <button class="bg-danger rounded-2 cursor-pointer" onclick="del(${i})">Delete</button>
+//                 <button class="bg-warning rounded-2 cursor-pointer" onclick="update(${i})">Update</button>
+//                 </div>
+            
+//             </div>`;
+//         } else {
+//             return ` <div class="d-flex justify-content-between">  <div  class="bg-primary rounded-2 my-3">${el}</div>
+//             <div  class="d-flex justify-content-between g-2">
+    
+//                 <button class="bg-danger rounded-2 cursor-pointer" onclick="del(${i})">Delete</button>
+//                 <button class="bg-warning rounded-2 cursor-pointer" onclick="edit(${i})">Edit</button>
+//                 </div>
+          
+//             </div>`;
+//         }
+
+//     }).join("");
+
+//     result.innerHTML = datamap;
+// }
+
+
+
+
+
+
+
+// btn.addEventListener("click", function () { //Add funksiyasi
+    
+//     data.push(inp.value);
+//     console.log(data);
+
+//     let datamap = data.reverse().map(function (el, i) {
+//         return ` <div class="d-flex justify-content-between">
+//         <div class="bg-primary rounded-2 my-3">${el}</div>
+//         <div class="d-flex justify-content-between g-2">
+
+//             <button class="bg-danger rounded-2 cursor-pointer" onclick="del(${i})">Delete</button>
+//             <button class="bg-warning rounded-2 cursor-pointer" onclick="edit(${i})">Edit</button>
+
+//         </div>
+//     </div>`;
+//     }).join("");
+
+//     result.innerHTML = datamap;
+// })
+
